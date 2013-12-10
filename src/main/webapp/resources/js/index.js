e7b7d2a6c0b1fad627f0eecf088de84316c73476
aqingsao <vinci.zhang@gmail.com>
2013-12-10 19:45:39 +0800

    use angularjs to search

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,24 +1,27 @@
-angular.module('grepcode', ['ngRoute', 'firebase'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        controller:'IndexCtrl',
-        templateUrl:'index.html'
-      })
-      .when('/list', {
-        controller:'ListCtrl',
-        templateUrl:'list.html'
-      })
-      .when('/view', {
-        controller:'ViewCtrl',
-        templateUrl:'detail.html'
-      })
-      .otherwise({
-        redirectTo:'/'
-      });
-  })
-  .controller('IndexCtrl', function($scope){
-    $scope.search = function(){
-      console.log("you are searching...");
-    }
-  });
+var app = angular.module('grepcode', []);
+//app.config(function($routeProvider) {
+//    $routeProvider
+//      .when('/index.html', {
+//        controller:'SearchCtrl',
+//        templateUrl:'index.html'
+//      })
+//      .when('/list', {
+//        controller:'ListCtrl',
+//        templateUrl:'list.html'
+//      })
+//      .when('/view', {
+//        controller:'ViewCtrl',
+//        templateUrl:'detail.html'
+//      })
+//      .otherwise({
+//        redirectTo:'/'
+//      });
+//  })
+app.controller('SearchCtrl', function($scope){
+  $scope.search = function(){
+    console.log("you are searching...");
+  }
+  $scope.sayHi = function(){
+    console.log("hi");
+  }
+});
